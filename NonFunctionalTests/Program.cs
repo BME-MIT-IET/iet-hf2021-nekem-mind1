6caf28a01271cc2e2060d7a6991b5134c38f482b
Viktor <nagy99vikotr@gmail.com>
2021-05-16 04:05:40 +0200

    add CreateRDFVariable benchmark

---
+++ NonFunctionalTests/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace NonFunctionalTests {
+    class Program {
+        static void Main(string[] args) {
+            BenchmarkRunner.Run<RDFSharpBenchmark>();
+        }
+    }
+}
---
+++ NonFunctionalTests/RDFSharpBenchmark.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using RDFSharp;
+using RDFSharp.Model;
+using RDFSharp.Query;
+
+namespace NonFunctionalTests {
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.Default)]
+    [RankColumn]
+    public class RDFSharpBenchmark {
+
+    
+        // public RDFVariable actor;
+        [Benchmark]
+        public void CreateRDFVariable() {
+            RDFVariable actor = new RDFVariable("actor");
+        }
+
+    }
+}
