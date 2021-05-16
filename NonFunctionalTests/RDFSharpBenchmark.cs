32021e80e61b12b8c21d5fc7527ea5536491ba4b
Viktor <nagy99vikotr@gmail.com>
2021-05-16 11:49:06 +0200

    add CreateRDFTripleWithRDFLiteral benchmark

--- NonFunctionalTests/RDFSharpBenchmark.cs
+++ NonFunctionalTests/RDFSharpBenchmark.cs
@@ -16,18 +16,22 @@ namespace NonFunctionalTests {
 
         RDFResource monaLisa;
         RDFResource creator;
+        RDFResource title;
         RDFResource leonardoDaVinci;
         RDFGraph museumGraph;
         RDFTriple monaLisaCreator;
+        RDFLiteral monaLisaTitle;
 
 
         public RDFSharpBenchmark() {
             this.monaLisa = new RDFResource("http://www.wikidata.org/entity/Q12418");
-            this.creator = new RDFResource("http://www.wikidata.org/entity/Q12418");
-            this.leonardoDaVinci = new RDFResource("http://www.wikidata.org/entity/Q12418");
+            this.creator = new RDFResource("http://purl.org/dc/terms/creator");
+            this.title = new RDFResource("http://purl.org/dc/terms/title");
+            this.leonardoDaVinci = new RDFResource("http://dbpedia.org/resource/Leonardo_da_Vinci");
             FileStream fs = File.OpenRead(@"..\..\..\..\..\..\..\\TestData\szepmuveszeti.n3");
             this.museumGraph = RDFGraph.FromStream(RDFModelEnums.RDFFormats.Turtle, fs);
             this.monaLisaCreator = new RDFTriple(this.monaLisa, this.creator, this.leonardoDaVinci);
+            this.monaLisaTitle = new RDFPlainLiteral("Mona Lisa");
         }
 
         [Benchmark]
@@ -43,6 +47,10 @@ namespace NonFunctionalTests {
         [Benchmark]
         public void CreateRDFTriple() {
             RDFTriple monaLisaCreator = new RDFTriple(this.monaLisa, this.creator, this.leonardoDaVinci);
+        }
+        [Benchmark]
+        public void CreateRDFTripleWithRDFLiteral() {
+            RDFTriple monaLisaCreator = new RDFTriple(this.monaLisa, this.title, this.monaLisaTitle);
         }
 
         [Benchmark]
