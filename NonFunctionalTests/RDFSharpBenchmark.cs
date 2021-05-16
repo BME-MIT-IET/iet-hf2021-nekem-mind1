573ff7a99d6d7a454dfc106b92a6029c20d4a2ab
Viktor <nagy99vikotr@gmail.com>
2021-05-16 11:13:25 +0200

    add  CreateRDFTriple and AddRDFTriple benchmark #4

--- NonFunctionalTests/RDFSharpBenchmark.cs
+++ NonFunctionalTests/RDFSharpBenchmark.cs
@@ -14,8 +14,22 @@ namespace NonFunctionalTests {
     [RankColumn]
     public class RDFSharpBenchmark {
 
-    
-        
+        RDFResource monaLisa;
+        RDFResource creator;
+        RDFResource leonardoDaVinci;
+        RDFGraph museumGraph;
+        RDFTriple monaLisaCreator;
+
+
+        public RDFSharpBenchmark() {
+            this.monaLisa = new RDFResource("http://www.wikidata.org/entity/Q12418");
+            this.creator = new RDFResource("http://www.wikidata.org/entity/Q12418");
+            this.leonardoDaVinci = new RDFResource("http://www.wikidata.org/entity/Q12418");
+            FileStream fs = File.OpenRead(@"..\..\..\..\..\..\..\\TestData\szepmuveszeti.n3");
+            this.museumGraph = RDFGraph.FromStream(RDFModelEnums.RDFFormats.Turtle, fs);
+            this.monaLisaCreator = new RDFTriple(this.monaLisa, this.creator, this.leonardoDaVinci);
+        }
+
         [Benchmark]
         public void CreateRDFVariable() {
             RDFVariable actor = new RDFVariable("actor");
@@ -23,7 +37,17 @@ namespace NonFunctionalTests {
 
         [Benchmark]
         public void CreateRDFResource() {
-            RDFResource actor = new RDFResource("http://www.wikidata.org/entity/Q12418");
+            RDFResource monaLisa = new RDFResource("http://www.wikidata.org/entity/Q12418");
+        }
+
+        [Benchmark]
+        public void CreateRDFTriple() {
+            RDFTriple monaLisaCreator = new RDFTriple(this.monaLisa, this.creator, this.leonardoDaVinci);
+        }
+
+        [Benchmark]
+        public void AddRDFTriple() {
+            this.museumGraph.AddTriple(monaLisaCreator);
         }
     }
 }
